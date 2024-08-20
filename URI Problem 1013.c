076b264f7dc582156fe12086bd26bcd00151fc55
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-08-21 01:56:39 +0600

    Beecrowd

---
+++ URI Problem 1013.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d%d",&a,&b,&c);
+    d=(a+b+abs(a-b))/2;
+    d=(d+c+abs(d-c))/2;
+    printf("%d eh o maior\n",d);
+    return 0;
+}
