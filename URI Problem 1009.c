0fbc92b084ea51731b61ee2e1dee336ba508a454
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-08-21 01:52:29 +0600

    Beecrowd

---
+++ URI Problem 1009.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char  employees_first_name[30];
+    double sellers_salary,total_value_sold,TOTAL;
+
+    gets( employees_first_name);
+    scanf("%lf%lf",&sellers_salary,&total_value_sold);
+
+    TOTAL=sellers_salary +(0.15*total_value_sold);
+    printf("TOTAL = R$ %.2lf\n",TOTAL);
+    return 0;
+
+}
