93320c5f13b4ba5bd1aa24df855b9bb2c9b533d3
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-08-21 01:54:32 +0600

    Beecrowd

---
+++ URI Problem 1011.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define pi 3.14159
+int main()
+{
+    double R,VOLUME;
+    scanf("%lf",&R);
+    VOLUME= (4/3.0) * pi * R * R * R ;
+    printf("VOLUME = %.3lf\n",VOLUME);
+    return 0;
+}
