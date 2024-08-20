f71f950ae07ae397f1a2e4b41dc971e68cc0ee89
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-08-21 02:02:08 +0600

    Beecrowd

---
+++ URI Problem 1015.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    double x1,x2,y1,y2,D;
+    scanf("%lf%lf%lf%lf",&x1,&y1,&x2,&y2);
+    D =sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    printf("%.4lf\n",D);
+    return 0;
+}
