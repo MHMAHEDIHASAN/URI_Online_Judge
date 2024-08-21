3a58c58b11746b6a62acb895d3c39f1826368a61
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-08-22 00:49:39 +0600

    Beecrowd

---
+++ URI Problem 1019.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int H,M,S;
+    scanf("%d",&S);
+    H=0;
+    M=0;
+    H=S/3600;
+    S=S%3600;
+    M=S/60;
+    S=S%60;
+
+      printf("%d:%d:%d\n",H,M,S);
+    return 0;
+}
