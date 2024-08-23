4517a3371b72205474742a049bb0679b7b9c046d
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-08-23 21:24:08 +0600

    Beecrowd

---
+++ URI Problem 1037.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    float N;
+    scanf("%f",&N);
+    if(N<0.00 || N>100.00)
+    {
+        printf("Fora de intervalo\n");
+    }
+    else if(N>=0.00 && N<=25.00)
+    {
+        printf("Intervalo [0,25]\n");
+    }
+    else if(N>=25.00 && N<=50.00)
+    {
+        printf("Intervalo (25,50]\n");
+    }
+    else if(N>=50.00 && N<=75.00)
+    {
+        printf("Intervalo (50,75]\n");
+    }
+    else if(N>=75.00 && N<=100.00)
+    {
+        printf("Intervalo (75,100]\n");
+    }
+    return 0;
+}
