e44948694f564da917542a99534e23c4e0e485b5
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-08-23 21:25:19 +0600

    Beecrowd

---
+++ URI Problem 1038.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y;
+    scanf("%d%d",&X,&Y);
+    float snack;
+    float  ar[5]= {4.00,4.50,5.00,2.00,1.50};
+    snack =ar[X-1]*Y;
+    printf("Total: R$ %.2f\n",snack);
+    return 0;
+}
