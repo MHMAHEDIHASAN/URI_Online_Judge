f1d51c08ee97f09998d1684e48a62490d41b6388
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-08-27 20:11:27 +0600

    URI Problem 1009.c

--- URI Problem 1009.c
+++ URI Problem 1009.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main()
-{
-    char  employees_first_name[30];
+{   char  employees_first_name[30];
     double sellers_salary,total_value_sold,TOTAL;
 
     gets( employees_first_name);
@@ -10,5 +9,4 @@ int main()
     TOTAL=sellers_salary +(0.15*total_value_sold);
     printf("TOTAL = R$ %.2lf\n",TOTAL);
     return 0;
-
 }
