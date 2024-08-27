1213d5798c51abb558be918cc20221d42d83d672
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-08-27 20:09:54 +0600

    URI Problem 1005.c

--- URI Problem 1005.c
+++ URI Problem 1005.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 int main()
-{
-    double A,B,MEDIA;
+{   double A,B,MEDIA;
     scanf("%lf %lf",&A,&B);
     A=A*3.5;
     B=B*7.5;
     MEDIA= (A+B)/11;
     printf("MEDIA = %.5lf\n",MEDIA);
     return 0;
-
 }
