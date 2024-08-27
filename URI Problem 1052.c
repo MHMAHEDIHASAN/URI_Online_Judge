f13aef73257ad931e465c4e667cafd836aafb6e8
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-08-27 20:17:17 +0600

    Beecrowd

---
+++ URI Problem 1052.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int num ;
+    scanf("%d",&num);
+    if (num ==1)
+        printf("January\n");
+    if (num==2)
+        printf("February\n");
+    if (num==3)
+        printf("March\n");
+    if (num==4)
+        printf("April\n");
+    if (num==5)
+        printf("May\n");
+    if (num==6)
+        printf("June\n");
+    if (num==7)
+        printf("July\n");
+    if (num==8)
+        printf("August\n");
+    if (num==9)
+        printf("September\n");
+    if (num==10)
+        printf("October\n");
+    if (num==11)
+        printf("November\n");
+    if (num==12)
+        printf("December\n");
+    return 0;
+}
