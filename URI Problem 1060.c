e81adc799070ac4e1181545825df4617708fdd09
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-08-27 20:22:22 +0600

    Beecrowd

---
+++ URI Problem 1060.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+int main()
+{
+    float x;
+    int i,sum=0;
+    for(i=1;i<=6;i++){
+        scanf("%f",&x);
+        if(x>0){
+        sum=sum+1;
+        }
+    }
+    printf("%d valores positivos\n",sum);
+    return 0;//end line
+}
