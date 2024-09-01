bf73dacdd4cb140c8b087f09b462232437070458
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-09-01 21:32:56 +0600

    Beecrowd

---
+++ URI Problem 1064.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    double n, sum = 0, avg;
+    int i, count = 0;
+    for(i = 1; i <= 6; i++)
+    {
+        scanf("%lf", &n);
+        if(n > 0)
+        {
+            sum += n;
+            count++;
+            avg = sum / count;
+        }
+    }
+    printf("%d valores positivos\n%.1lf\n", count, avg);
+
+    return 0;
+}
