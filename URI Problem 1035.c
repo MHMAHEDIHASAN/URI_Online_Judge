0233fdd5e03c5062449641c9504311d02e215421
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-09-08 00:57:25 +0600

    Beecrowd

--- URI Problem 1035.c
+++ URI Problem 1035.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
-{   int A,B,C,D;
+{   
+    int A,B,C,D;
     scanf("%d %d %d %d",&A,&B,&C,&D);
     if((B>C) && (D>A) &&(C+D > A+B) && (C>=0 && D>=0) && (A%2==0))
     {
