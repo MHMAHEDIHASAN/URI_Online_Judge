f40f2dd3dc4664a230b68be5eaa3e50bae65e8d9
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-09-13 00:23:54 +0600

    Beecrowd

--- URI Problem 1007.c
+++ URI Problem 1007.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main()
-{   
-    int A,B,C,D,DIFERENCA;
+{   int A,B,C,D,DIFERENCA;
     scanf("%d%d%d%d",&A,&B,&C,&D);
     DIFERENCA=(A * B - C * D);
     printf("DIFERENCA = %d\n",DIFERENCA);
